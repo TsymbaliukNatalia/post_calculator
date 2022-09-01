8f8152405d0a27aec7c76540838c50b2f3420b02
TsymbaliukNatalia <tsymbaliuk.natalia20@gmail.com>
2022-09-01 12:57:42 +0300

    Add Craftable and NewPost packages

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Daaner\NovaPoshta\NovaPoshtaServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -229,6 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'NovaPoshta' => Daaner\NovaPoshta\Facades\NovaPoshta::class,
 
     ],
 
